84846957ed868b1543ea9a79afa1072dbb4340c1
bilgekagansenol <bilge.kagan.senoll@gmail.com>
2025-04-15 16:48:30 +0300

    few regulation on router

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -127,3 +127,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user_api.UserProfile'
+
+#deneme
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
